69f390297d4e509c2618983d34da459e783724ab
Juan Pablo Inzunza Alfaro MX.com <juan.inzunza@mexico.org>
2018-06-25 16:03:48 -0500

    Datos adjuntos

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+const card1 = document.querySelector('#tarjeta1');
+
+card1.addEventListener( 'click', function() {
+  card1.classList.toggle('is-flipped');
+});
+
+const card2 = document.querySelector('#tarjeta2');
+
+card2.addEventListener( 'click', function() {
+  card2.classList.toggle('is-flipped');
+});
+
+const card3 = document.querySelector('#tarjeta3');
+
+card3.addEventListener( 'click', function() {
+  card3.classList.toggle('is-flipped');
+});
+
+const card4 = document.querySelector('#tarjeta4');
+
+card4.addEventListener( 'click', function() {
+  card4.classList.toggle('is-flipped');
+});
+
+
+
+
+
+
+
+
+
+
+
+
