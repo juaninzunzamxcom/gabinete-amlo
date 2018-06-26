47f7814a158913ccd4ddfb0af9dcdad74f81944a
Alberto Gómez <alberto@mexico.org>
2018-06-26 11:53:28 +0200

    Ajustes de JS y Framework CSS

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
-
-
-
-const card1 = document.querySelector('#tarjeta1');
+/*const card1 = document.querySelector('#tarjeta1');
 
 card1.addEventListener( 'click', function() {
   card1.classList.toggle('is-flipped');
@@ -23,7 +20,16 @@ const card4 = document.querySelector('#tarjeta4');
 
 card4.addEventListener( 'click', function() {
   card4.classList.toggle('is-flipped');
-});
+});*/
+
+var cards = document.getElementsByClassName('tarjeta');
+
+for(i = 0;i < cards.length; i++) {
+  var card = cards[i];
+  card.addEventListener( 'click', function() {
+    this.classList.toggle('is-flipped');
+  });
+}
 
 
 
